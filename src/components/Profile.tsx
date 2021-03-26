bfc76118aabae24c366c7b07e4346fb2e2dc5f16
ThiagoAZ <thiagoaugustozeferino@gmail.com>
2021-03-26 03:28:01 -0300

    :zap:  2.0

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -11,23 +11,29 @@ export function Profile() {
     const { level, ready, turnReadyOn } = useContext(ChallengesContext)
     const { isActive } = useContext(CountdownContext)
     const [state, setState] = useState('plus.png')
+    
+    
 
-    const AddNew = ({}) => {
-        const [file, setFile] = React.useState(null)
-        
-        const fileHandler = (e) => {
-            setFile(e.target.files[0])
-        }
-        
-        return (
+    let size_screen
+
+    if (typeof window !== "undefined") {
+        // browser code
+        size_screen = window.innerWidth
+    }
+
+    return (
+
+        <div id={styles.root}>
+            {size_screen <= 430 ? (
+                <>
+                {ready && (
+                    <div className = {styles.profileContainer}>
                     <div>
                         <div className={styles.personal_image}>
                             <label className={styles.label}>
-                            <input type="file" onChange={fileHandler} />
                                     <figure className={styles.personal_figure}>
 
-                                    <img src={file? URL.createObjectURL(file) : null} alt={file? file.name : null} id={styles.personal_avatar}/>
-
+                                        <img src="" alt=""/>
 
                                         <figcaption className={styles.personal_figcaption}>
 
@@ -38,30 +44,6 @@ export function Profile() {
                             </label>
                         </div>
                     </div>
-
-        )
-    }
-    
-    
-    
-
-
-    let size_screen
-
-    if (typeof window !== "undefined") {
-        // browser code
-        size_screen = window.innerWidth
-        ReactDOM.render(<AddNew />, document.getElementById('root'));
-    }
-
-    return (
-
-        <div id={styles.root}>
-            {size_screen <= 430 ? (
-                <>
-                {ready && (
-                    <div className = {styles.profileContainer}>
-                        <AddNew />
                         <div>
                             <strong>You</strong>
                             <p>
@@ -76,7 +58,22 @@ export function Profile() {
             ) : (
 
                     <div className = {styles.profileContainer}>
-                    <AddNew />
+                    <div>
+                        <div className={styles.personal_image}>
+                            <label className={styles.label}>
+                                    <figure className={styles.personal_figure}>
+
+                                        <img src="" alt=""/>
+
+                                        <figcaption className={styles.personal_figcaption}>
+
+                                            <img src="https://raw.githubusercontent.com/ThiagoLuizNunes/angular-boilerplate/master/src/assets/imgs/camera-white.png" />
+
+                                        </figcaption>
+                                    </figure>
+                            </label>
+                        </div>
+                    </div>
                         <div>
                             <strong>You</strong>
                             <p>
